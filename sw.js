653d8153d6a734977ebb3a4025d72b7f4ceb427e
AGH21331 <aghilesdjioua@gmail.com>
2026-08-11 21:08:41 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='smartmeal-v10';
+const CACHE='smartmeal-v11';
 const CORE=['./','./index.html','./manifest.json','./smartmeal-icon-192.png','./smartmeal-icon-512.png'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
